0e8cba0e688a377099b1d4b1aab03ca977160331
RodriMaldonadoMedina <maldonadomedinarodrigo@gmail.com>
2022-07-13 14:42:00 +0000

    mi primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["the dog", "my granma", "his turtle", "my bird"];
+  let que = ["eat", "pissed", "crushed", "broked"];
+  let cuando = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generarRandom(totalExcusas) {
+    return Math.floor(Math.random() * totalExcusas);
+  }
+  //Genero el sujeto de la excusa
+  let posicionSujeto = generarRandom(quien.length);
+  let sujeto = quien[posicionSujeto];
+
+  //Genero el que paso
+  let posicionQue = generarRandom(que.length);
+  let quePaso = que[posicionQue];
+
+  //Genero el cuando paso
+  let posicionCuando = generarRandom(cuando.length);
+  let cuandoPaso = cuando[posicionQue];
+
+  let parrafo = document.getElementById("excuse");
+  parrafo.innerHTML = sujeto + " " + quePaso + " " + cuandoPaso;
 };
